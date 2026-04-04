f3849076c3c1d533e7715f301295e57a59530f0a
milton Ayca <miltonaycacolque1@gmail.com>
2026-04-04 13:26:30 -0400

    chore: actualizar rutas para HU-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import ProfilePage from './pages/ProfilePage';
-import './App.css';
-import LoginPage from './pages/auth/LoginPage';//Este es tu login oe milton
-import Navbar from './components/ui/Navbar';
+import AppRouter from "./routes/AppRouter";
 
 function App() {
-  const HomePage = () => (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>Bienvenido a Nexum Frontend</h1>
-      <p>Este es un ejemplo de la página de inicio.</p>
-      <nav style={{ marginTop: '20px' }}>
-        <Link to="/profile" style={{ padding: '10px 20px', border: '1px solid blue', borderRadius: '5px', textDecoration: 'none', color: 'blue' }}>
-          Ir a Datos Personales
-        </Link>
-      </nav>
-    </div>
-  );
-
-  return (
-    <Router>
-      <Navbar />
-      <div style={{ flexGrow: 1 }}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <AppRouter />;
 }
 
 export default App;
