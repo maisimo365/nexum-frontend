a347295f4c7a9ee111a5f5d1913295deb3723973
Miguel Angel Massi Geronimo <miguelangelmassigeronimo@gmail.com>
2026-04-04 10:08:36 -0400

    feat: Se agrego redirecccion al home con el logo

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -4,17 +4,19 @@ import logoUmss from "../../assets/logoUmss.png";
 const Navbar = () => {
   return (
     <nav className="w-full bg-navbar px-6 py-3 flex items-center justify-between">
-      {/* Logo */}
-      <div className="flex items-center gap-2">
-        <img
-              src={logoUmss}
-              alt="Logo UMSS"
-               className="w-8 h-8 object-contain"
-             />
-        <span className="text-white font-bold text-lg tracking-wide">
-          NEXUM
-        </span>
-      </div>
+      {/* Logo y nombre de Nexum */}
+      <Link to="/" className="flex items-center gap-2 cursor-pointer">
+        <div className="flex items-center gap-2">
+          <img
+                src={logoUmss}
+                alt="Logo UMSS"
+                 className="w-8 h-8 object-contain"
+               />
+          <span className="text-white font-bold text-lg tracking-wide">
+            NEXUM
+          </span>
+        </div>
+      </Link>
 
       {/* Botones */}
       <div className="flex items-center gap-3">
